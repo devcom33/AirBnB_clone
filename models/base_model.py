cb89b0a86ce64c3697d14dbfd9fefd34700f02f3
devcom33 <mouad.elb06@gmail.com>
2024-01-24 00:50:44 -0800

    test

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,21 @@ import uuid
 from datetime import datetime
 """BaseModel"""
 
+
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Base Model Instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    self.__dict__[k] = datetime\
+                                    .strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                elif k == 'id':
+                    self.id = v
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Update the 'updated_at' timestamp to the current datetime."""
@@ -16,7 +25,7 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
-        class_name = self.__class__.__name_
+        class_name = self.__class__.__name__
         created_at_str = self.created_at.isoformat()
         updated_at_str = self.updated_at.isoformat()
         return {'id': self.id,
