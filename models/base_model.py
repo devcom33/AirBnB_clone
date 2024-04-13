d536779f10896a5eb534bb7942a39a4aaff74bb1
devcom33 <mouad.elb06@gmail.com>
2024-04-13 11:02:09 +0000

    upd this err

--- models/base_model.py
+++ models/base_model.py
@@ -16,24 +16,25 @@ class BaseModel:
                     self.id = v
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
     def save(self):
         """Update the 'updated_at' timestamp to the current datetime."""
-        self.updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values"""
+        dict_obj = self.__dict__.copy()
         class_name = self.__class__.__name__
-        created_at_str = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        updated_at_str = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return {'__class__': class_name,
-                'id': self.id,
-                'created_at': created_at_str,
-                'updated_at': updated_at_str,
-                **self.__dict__
-                }
+        created_at_ = self.created_at.isoformat()
+        updated_at_ = self.updated_at.isoformat()
+        dict_obj['__class__'] = class_name
+        dict_obj['id'] = self.id
+        dict_obj['created_at'] = created_at_
+        dict_obj['updated_at'] = updated_at_
+        return dict_obj
+
     def __str__(self):
         """Return string representation of the object."""
         return "[{}] ({}) {}".format(
@@ -42,6 +43,3 @@ class BaseModel:
             self.__dict__
         )
 
-    def __repr__(self):
-        """Return object representation."""
-        return "<{}>".format(str(self))
