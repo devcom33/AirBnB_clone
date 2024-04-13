f23b659bc780882b0014b8f6f7cd08bafd0a026d
devcom33 <mouad.elb06@gmail.com>
2024-04-13 10:25:10 +0000

    test

--- models/base_model_.py
+++ models/base_model_.py
@@ -1,15 +1,19 @@
-mport uuid
+#!/usr/bin/python3
+import uuid
 from datetime import datetime
+"""BaseModel"""
+
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """Base Model Instance"""
-        if kwargs:
+        if len(kwargs) > 0:
             for k, v in kwargs.items():
                 if k in ["created_at", "updated_at"]:
-                    setattr(self, k, datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f'))
+                    self.__dict__[k] = datetime\
+                                    .strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 elif k == 'id':
-                    setattr(self, k, v)
+                    self.id = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -20,18 +24,16 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values"""
+        """returns a dictionary containing all keys/values"""
         class_name = self.__class__.__name__
         created_at_str = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
         updated_at_str = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return {
-            '__class__': class_name,
-            'id': self.id,
-            'created_at': created_at_str,
-            'updated_at': updated_at_str,
-            **self.__dict__
-        }
-
+        return {'__class__': class_name,
+                'id': self.id,
+                'created_at': created_at_str,
+                'updated_at': updated_at_str,
+                **self.__dict__
+                }
     def __str__(self):
         """Return string representation of the object."""
         return "[{}] ({}) {}".format(
